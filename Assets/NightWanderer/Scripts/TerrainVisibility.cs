462ce2003609fb80bcfa3098301103d4aefc3e72
Dmitry-Stukalov <dimastukalov04@gmail.com>
2026-04-13 15:56:33 +0300

    Added new trees

---
+++ Assets/NightWanderer/Scripts/TerrainVisibility.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TerrainVisibility : MonoBehaviour
+{
+	[SerializeField] private Camera _camera;
+	private Terrain _terrain;
+	private TerrainCollider _terrainCollider;
+
+	private void Start()
+	{
+		_terrain = GetComponent<Terrain>();
+		_terrainCollider = GetComponent<TerrainCollider>();
+	}
+
+	private void LateUpdate()
+	{
+		if (Vector3.Distance(transform.position, _camera.transform.position) >= 1500)
+		{
+			_terrain.enabled = false;
+			_terrainCollider.enabled = false;
+		}
+		else
+		{
+			_terrain.enabled = true;
+			_terrainCollider.enabled = true;
+		}
+	}
+}
