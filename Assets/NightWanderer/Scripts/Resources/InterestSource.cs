65ce52a2c6bfc06074ce089a4c66bb2d71a7065d
Dmitry-Stukalov <dimastukalov04@gmail.com>
2026-06-12 18:01:53 +0300

    Added interesting stone

---
+++ Assets/NightWanderer/Scripts/Resources/InterestSource.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditorInternal;
+
+public class InterestSource : MonoBehaviour
+{
+	[SerializeField] private Material _dayMaterial;
+	[SerializeField] private Material _nightMaterial;
+	[SerializeField] private int _showCrystalsCount;
+	private List<GameObject> _crystals = new List<GameObject>();
+	private List<MeshRenderer> _oreMaterial = new List<MeshRenderer>();
+	private Sun _sun;
+	private Timer _recoveryTimer;
+	private int _currentCrystalCount;
+
+	private void Start()
+	{
+		foreach (Transform obj in transform) _crystals.Add(obj.gameObject);
+		_crystals.RemoveAt(0);
+
+		for (int i = 0; i < _crystals.Count; i++) _oreMaterial.Add(_crystals[i].GetComponent<MeshRenderer>());
+
+		_crystals.RemoveAt(_crystals.Count - 1);
+
+		_currentCrystalCount = _crystals.Count;
+
+		_recoveryTimer = new Timer(120);
+		_recoveryTimer.OnTimerEnd += RecoveryCrystal;
+
+		_sun = FindAnyObjectByType<Sun>();
+		_sun.OnDayStart += () =>
+		{
+			for (int i = 0; i < _oreMaterial.Count; i++) _oreMaterial[i].material = _dayMaterial;
+		};
+
+		_sun.OnNightStart += () =>
+		{
+			for (int i = 0; i < _oreMaterial.Count; i++) _oreMaterial[i].material = _nightMaterial;
+		};
+
+		for (int i = 0; i < _oreMaterial.Count; i++) _oreMaterial[i].material = _nightMaterial;
+
+		HideRandomCrystals();
+	}
+
+	private void HideRandomCrystals()
+	{
+		int t = _crystals.Count - _showCrystalsCount;
+		int randomNumber = 0;
+
+		while (t > 0)
+		{
+			randomNumber = Random.Range(0, _crystals.Count);
+
+			if (_crystals[randomNumber].activeSelf)
+			{
+				_crystals[randomNumber].SetActive(false);
+				_currentCrystalCount--;
+				t--;
+			}
+		}
+	}
+
+	private void RecoveryCrystal()
+	{
+		int randomNumber = 0;
+
+		if (_currentCrystalCount < _showCrystalsCount)
+		{
+			while(true)
+			{
+				randomNumber = Random.Range(0, _crystals.Count);
+
+				if (!_crystals[randomNumber].activeSelf)
+				{
+					_crystals[randomNumber].SetActive(true);
+					_currentCrystalCount++;
+					break;
+				}
+			}
+		}
+
+		_recoveryTimer.ResetTimer(false);
+	}
+
+	private void Update()
+	{
+		_recoveryTimer?.Tick(Time.deltaTime);
+	}
+
+	private void OnDisable()
+	{
+		_sun.OnDayStart -= () =>
+		{
+			for (int i = 0; i < _oreMaterial.Count; i++) _oreMaterial[i].material = _dayMaterial;
+		};
+
+		_sun.OnNightStart -= () =>
+		{
+			for (int i = 0; i < _oreMaterial.Count; i++) _oreMaterial[i].material = _nightMaterial;
+		};
+	}
+}
---
+++ Assets/NightWanderer/Scripts/Resources/KnockedDownResource.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class KnockedDownResource : MonoBehaviour
+{
+	[SerializeField] private ResourceLibrary _resourceLibrary;
+	[SerializeField] private VisualEffect _knockedDownEffect;
+	[SerializeField] private int _resourceID;
+	[SerializeField] private int _minResourceCapacity;
+	[SerializeField] private int _maxResourceCapacity;
+
+	private void Start()
+	{
+		_resourceLibrary = FindAnyObjectByType<ResourceLibrary>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			_knockedDownEffect.Play();
+			StartCoroutine(DeathCoroutine());
+
+			int randomCapacity = UnityEngine.Random.Range(_minResourceCapacity, _maxResourceCapacity + 1);
+
+			GameObject resource = _resourceLibrary.GetResource(_resourceID);
+			resource.transform.SetParent(gameObject.transform.parent, true);
+			resource.GetComponent<ResourceOnLand>().SetResourceCount(randomCapacity);
+			resource.transform.position = transform.position;
+
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+			gameObject.GetComponent<BoxCollider>().enabled = false;
+		}
+	}
+
+	private IEnumerator DeathCoroutine()
+	{
+		yield return new WaitForSeconds(0.3f);
+
+		gameObject.GetComponent<MeshRenderer>().enabled = true;
+		gameObject.GetComponent<BoxCollider>().enabled = true;
+		gameObject.SetActive(false);
+	}
+}
